6653cd59d77609ffffc22c68790162e72ec9c960
Shuu Wolf <shuuwolf.home@gmail.com>
2022-04-23 10:28:24 -0300

    Propriedades no React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/'
+}
 
 export function RepositoryList() {
     return (
@@ -6,50 +12,10 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <li>
-                    <strong>{repositoryName}</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
